0d0063c29e53ca6b9272084d7bb75e38f3acf25f
Bopas <nazihbopas@gmail.com>
2017-07-08 22:40:25 +0700

    update
    update

--- wp-content/themes/theme-onphpid-master-juni/footer.php
+++ wp-content/themes/theme-onphpid-master-juni/footer.php
@@ -1,40 +1,58 @@
 <!-- Footer -->
-    <footer class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    <ul class="list-inline text-center">
-                        <li>
-                            <a href="#">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <span class="fa-stack fa-lg">
-                                    <i class="fa fa-circle fa-stack-2x"></i>
-                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
-                                </span>
-                            </a>
-                        </li>
-                    </ul>
-                    <br/>
-                    <p class="copyright text-muted" style="color: #333;">Copyright &copy; Kominfo-2017</p>
-                </div>
-            </div>
-        </div>
-    </footer>
+<footer id="footer-Section">
     <?php wp_footer();?>
+    <div class="footer-top-layout">
+        <div class="container">
+          <div class="row">
+            <div class="OurBlog">
+              <h4>Our Blog</h4>
+              <p>BCM wins mandate for Avendus Wealth Management portal revamp</p>
+              <div class="post-blog-date">20th Oct 2016</div>
+          </div>
+          <div class=" col-lg-10 col-lg-offset-1">
+              <div class="col-sm-4">
+                <div class="footer-col-item">
+                  <h4>Go Travel Cloud</h4>
+                  <address>
+                      501,507 your company address<br>
+                      400015 Maharashtra, UK
+                  </address>
+              </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="footer-col-item">
+              <h4>Reach Us</h4>
+              <div class="item-contact"> 
+              <a href="tel:630-885-9200"><span class="link-id">P</span>:<span class="footer_contact">630-885-9200</span></a> 
+              <a href="tel:630-839.2006" ><span class="link-id">F</span>:<span class="footer_contact">630-839.2006</span></a> 
+              <a href="mailto:info@brandcatmedia.com"><span class="link-id">E</span>:<span class="footer_contact">info@brandcatmedia.com</span></a> 
+              </div>
+          </div>
+      </div>
+      <div class="col-sm-4">
+        <div class="footer-col-item">
+          <h4>Sign up for Newsletter</h4>
+          <form class="signUpNewsletter" action="" method="get">
+            <input name="" class="gt-email form-control" placeholder="You@youremail.com" type="text">
+            <input name="" class="btn-go" value="Go" type="button">
+        </form>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="footer-bottom-layout">
+    <div class="socialMedia-footer"> 
+        <a href="#"><img src="<?php echo get_template_directory_uri() .'/img/icon/ICON/facebook.png';?>"></a> 
+        <a href="#"><img src="<?php echo get_template_directory_uri() .'/img/icon/ICON/twitter.png';?>"></a> 
+        <a href="#"><img src="<?php echo get_template_directory_uri() .'/img/icon/ICON/linkedin.png';?>"></a> 
+        <a href="#"><img src="<?php echo get_template_directory_uri() .'/img/icon/ICON/youtube.png';?>"></a> 
+        <a href="#"><img src="<?php echo get_template_directory_uri() .'/img/icon/ICON/mail.png';?>"></a> </div>
+        <div class="copyright-tag">Copyright &copy; Kominfo-2017</div>
+    </div>
+</footer>
+
+
 </body>
 </html>
